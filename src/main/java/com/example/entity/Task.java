f91f5802fbd00521ac222e63a43ea92c6f9b1ac5
Mark Ian Ebuen <ianebuen2001@gmail.com>
2025-09-26 15:54:31 +0800

    Create a project structure

--- src/main/java/com/example/entity/Task.java
+++ src/main/java/com/example/entity/Task.java
@@ -1,4 +1,25 @@
 package com.example.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+    private String priority;
+    private Long userId;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+
 }
